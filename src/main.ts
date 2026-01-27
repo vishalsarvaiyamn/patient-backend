6a72594fa2aea010d5374f2744ea11e2c30297fc
Vishal Sarvaiya <vishalsarvaiya@medianv.com>
2026-01-27 18:13:55 +0530

    feat(app): Added cors configs

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
